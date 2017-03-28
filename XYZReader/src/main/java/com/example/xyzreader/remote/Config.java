30f77c9e951cf6c7f2d082c311cef8a6795dad0b
Lam Bui <lamdbui@gmail.com>
2017-03-28 11:36:23 -0700

    Did some cleanup and switch to a better JSON file

--- XYZReader/src/main/java/com/example/xyzreader/remote/Config.java
+++ XYZReader/src/main/java/com/example/xyzreader/remote/Config.java
@@ -10,8 +10,8 @@ public class Config {
         URL url = null;
         try {
             //url = new URL("https://dl.dropboxusercontent.com/u/231329/xyzreader_data/data.json" );
-            //url = new URL("https://nspf.github.io/XYZReader/data.json");
-            url = new URL("https://gist.githubusercontent.com/AtomicSpider/12b731e3e2a41f336cc1bd7fc904efbb/raw/3e6da3480b132f6d0eb99ca3711da4a2c901627e/data.json");
+            url = new URL("https://nspf.github.io/XYZReader/data.json");
+            //url = new URL("https://gist.githubusercontent.com/AtomicSpider/12b731e3e2a41f336cc1bd7fc904efbb/raw/3e6da3480b132f6d0eb99ca3711da4a2c901627e/data.json");
         } catch (MalformedURLException ignored) {
             // TODO: throw a real error
         }
